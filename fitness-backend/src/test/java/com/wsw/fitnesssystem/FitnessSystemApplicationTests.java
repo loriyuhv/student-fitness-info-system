2fc216ab81afacdec6fac87fd90cb6d7aa313516
loriyuhv <2385923602@qq.com>
2026-01-13 20:13:06 +0800

    chore(fitness-backend): 初始化Spring Boot基础工程

---
+++ fitness-backend/src/test/java/com/wsw/fitnesssystem/FitnessSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wsw.fitnesssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello Spring Boot!");
+    }
+
+}
